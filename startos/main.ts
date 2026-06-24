52cf7434c83d8f72739a03625def5ad87b90d955
William K. Santiago <william@privkey.io>
2026-06-24 18:44:50 -0400

    Revert to port-listening healthcheck to avoid boot log noise

--- startos/main.ts
+++ startos/main.ts
@@ -41,14 +41,10 @@ export const main = sdk.setupMain(async ({ effects }) => {
     ready: {
       display: i18n('Server API'),
       fn: () =>
-        sdk.healthCheck.checkWebUrl(
-          effects,
-          `http://localhost:${uiPort}/healthcheck`,
-          {
-            successMessage: i18n('The Standard Notes Server is ready'),
-            errorMessage: i18n('The Standard Notes Server is not responding'),
-          },
-        ),
+        sdk.healthCheck.checkPortListening(effects, uiPort, {
+          successMessage: i18n('The Standard Notes Server is ready'),
+          errorMessage: i18n('The Standard Notes Server is not responding'),
+        }),
     },
     requires: [],
   })
